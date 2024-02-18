90ba880169d7240d3a70a8ec6782ec2433eab0a6
ishawant kumar <ishawantkumar@gmail.com>
2024-02-18 14:22:35 +0530

    data ingestion compenet updated

--- Xray/constant/__init__.py
+++ Xray/constant/__init__.py
@@ -5,11 +5,15 @@ import torch
 
 TIMESTAMP: datetime=datetime.now().strftime("%m_%d_%Y_%H_%M_%S") 
 
+# Data ingestion related constant
+
 ARTIFACT_DIR: str = 'artifacts'
 
-AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
-REGION_NAME = "us-east-1"
+
+# AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+# AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+# REGION_NAME = "us-east-1"
+
 
 BUCKET_NAME: str = "lungxray1"
 
