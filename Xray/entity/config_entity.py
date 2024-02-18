6ead1f4358e9477f0cd210bc0633b9311405a88b
ishawant kumar <ishawantkumar@gmail.com>
2024-02-18 14:22:24 +0530

    conifg entity updated for data ingestion

--- Xray/entity/config_entity.py
+++ Xray/entity/config_entity.py
@@ -1,22 +1,24 @@
 import os 
 from dataclasses import dataclass 
 from torch import device 
-from Xray.constant.training_pipeline import * 
+from Xray.constant import * 
 
 @dataclass
 class DataIngestionConfig:
     def __init__(self):
-        self.S3_data_folder: str = S3_DATA_FOLDER 
-        self.bucket_name: str = BUCKET_NAME 
+        self.s3_data_folder: str = S3_DATA_FOLDER
 
-        self.artifact_dir: str = os.path.join(ARTIFACT_DIR,TIMESTAMP)
+        self.bucket_name: str = BUCKET_NAME
+
+        self.artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
 
         self.data_path: str = os.path.join(
-            self.artifact_dir,"data_ingestion".self.S3_data_folder
+            self.artifact_dir, "data_ingestion", self.s3_data_folder
         )
 
-        self.train_data_path: str = os.path.join(self.data_path,'train') 
-        self.test_data_path: str = os.path.join(self.data_path,'test')
+        self.train_data_path: str = os.path.join(self.data_path, "train")
+
+        self.test_data_path: str = os.path.join(self.data_path, "test")
 
 @dataclass
 class DataTransformationConfig:
