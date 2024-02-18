2da73b5c15fc5af8d58daafdb5cfa63e0230b46a
ishawant kumar <ishawantkumar@gmail.com>
2024-02-18 14:26:41 +0530

    data ingestion updated

--- Xray/entity/artifact_entity.py
+++ Xray/entity/artifact_entity.py
@@ -1,20 +1,9 @@
 from dataclasses import dataclass
 from torch.utils.data.dataloader import DataLoader
-# from Xray.logger import logger
-# from Xray.exception import XrayException
-# from Xray.constant.training_pipeline import * 
+
 
 @dataclass
 class DataIngestionArtifact:
     train_file_path: str
-    test_file_path: str 
 
-@dataclass
-class DataTransformationArtifact:
-    transformed_train_object: DataLoader
-
-    transformed_test_object: DataLoader
-
-    train_transform_file_path: str
-
-    test_transform_file_path: str
+    test_file_path: str
