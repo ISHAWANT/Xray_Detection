7c42baf57973a6a05dada0c31a5eba97017fc5af
ishawant kumar <ishawantkumar@gmail.com>
2024-02-18 15:33:55 +0530

    demo.py updated

--- demo.py
+++ demo.py
@@ -2,12 +2,20 @@
 # ins = S3Client() 
 
 # Data Ingestion 
-from Xray.entity.config_entity import DataIngestionConfig
-from Xray.entity.artifact_entity import DataIngestionArtifact 
+# from Xray.entity.config_entity import DataIngestionConfig
+# from Xray.entity.artifact_entity import DataIngestionArtifact 
 
-from Xray.components.data_ingestion import DataIngestion 
+# from Xray.components.data_ingestion import DataIngestion 
 
-di_ins = DataIngestion(DataIngestionConfig)
-di_art = di_ins.initiate_data_ingestion() 
+# di_ins = DataIngestion(DataIngestionConfig)
+# di_art = di_ins.initiate_data_ingestion() 
+
+from Xray.pipeline.training_pipeline import TrainingPipeline 
+
+if __name__=='__main__':
+    training_pipeline = TrainingPipeline() 
+    training_pipeline.run_pipeline() 
+
+
 
 
