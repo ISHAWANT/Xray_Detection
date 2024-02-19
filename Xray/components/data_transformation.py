754a8ee3b2c940d94088b8c691c23da406317b3d
ishawant kumar <ishawantkumar@gmail.com>
2024-02-19 09:26:58 +0530

    data_transformation udpated

--- Xray/components/data_transformation.py
+++ Xray/components/data_transformation.py
@@ -7,13 +7,14 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
-from xray.entity.artifacts_entity import (
+from Xray.entity.artifact_entity import (
     DataIngestionArtifact,
     DataTransformationArtifact,
 )
-from xray.entity.config_entity import DataTransformationConfig
-from xray.exception import XRayException
-from xray.logger import logging
+from Xray.entity.config_entity import DataTransformationConfig
+from Xray.exception import XrayException
+from Xray.logger import logging
+
 
 
 class DataTransformation:
@@ -57,7 +58,9 @@ class DataTransformation:
             return train_transform
 
         except Exception as e:
-            raise XRayException(e, sys)
+            raise XrayException(e, sys)
+        
+
 
     def transforming_testing_data(self) -> transforms.Compose:
         logging.info(
@@ -83,61 +86,83 @@ class DataTransformation:
             return test_transform
 
         except Exception as e:
-            raise XRayException(e, sys)
+            raise XrayException(e, sys)
+        
 
-    def data_loader(self,train_transform:transforms.Compose,test_transform:transforms.Compose)-> Tuple[DataLoader,DataLoader]:
+
+    
+    def data_loader(
+        self, train_transform: transforms.Compose, test_transform: transforms.Compose
+    ) -> Tuple[DataLoader, DataLoader]:
         try:
-            logging.info('Entered the data loader method of data transformation class')
+            logging.info("Entered the data_loader method of Data transformation class")
+
             train_data: Dataset = ImageFolder(
-                os.path.join(self,data_ingestion_artifact.train_file_path),
-                transform=train_transform
+                os.path.join(self.data_ingestion_artifact.train_file_path),
+                transform=train_transform,
             )
+
             test_data: Dataset = ImageFolder(
                 os.path.join(self.data_ingestion_artifact.test_file_path),
-                transform=test_transform
+                transform=test_transform,
             )
 
-            logging.info('Created train data and test data paths') 
+            logging.info("Created train data and test data paths")
 
             train_loader: DataLoader = DataLoader(
                 train_data, **self.data_transformation_config.data_loader_params
             )
+
             test_loader: DataLoader = DataLoader(
                 test_data, **self.data_transformation_config.data_loader_params
             )
 
-            logging.info("Exited the data loader method of data transformation class")
-            return train_loader,test_loader 
+            logging.info("Exited the data_loader method of Data transformation class")
 
+            return train_loader, test_loader
 
         except Exception as e:
-            raise XrayException(e,sys)
+            raise XrayException(e, sys)
+        
 
-    def initiate_data_transformation(self)->DataTransformationArtifact: 
+
+    
+    def initiate_data_transformation(self) -> DataTransformationArtifact:
         try:
-            logging.info("Entered the initiate data_transforamtion method of data transfomation class")
+            logging.info(
+                "Entered the initiate_data_transformation method of Data transformation class"
+            )
 
-            train_transform: transform.Compose = self.transforming_testing_data()
-            test_transform: transform.Compose = self.transforming_testing_data() 
-            
-            os.makedirs(self.data_transformation_config.artifact_dir,exit_ok=True) 
+            train_transform: transforms.Compose = self.transforming_training_data()
+
+            test_transform: transforms.Compose = self.transforming_testing_data()
+
+            os.makedirs(self.data_transformation_config.artifact_dir, exist_ok=True)
 
             joblib.dump(
-                train_transform,self.data_transformation_config.test_transforms_file
+                train_transform, self.data_transformation_config.train_transforms_file
             )
-            joblib.dump(
-                test_transform,sefl.data_transformation_config.test_transforms_file
-            )
-            logging.info("Exited the initiate_data_transformation method of data transformation class") 
 
-            return data_transformation_artifact 
+            joblib.dump(
+                test_transform, self.data_transformation_config.test_transforms_file
+            )
+
+            train_loader, test_loader = self.data_loader(
+                train_transform=train_transform, test_transform=test_transform
+            )
+
+            data_transformation_artifact: DataTransformationArtifact = DataTransformationArtifact(
+                transformed_train_object=train_loader,
+                transformed_test_object=test_loader,
+                train_transform_file_path=self.data_transformation_config.train_transforms_file,
+                test_transform_file_path=self.data_transformation_config.test_transforms_file,
+            )
+
+            logging.info(
+                "Exited the initiate_data_transformation method of Data transformation class"
+            )
+
+            return data_transformation_artifact
 
         except Exception as e:
-            raise XrayException(e,sys) 
-            loggin.info('Data transformation completed!!!!') 
-            loggin.info('code runner checked and exits from main component loop') 
-
-            
-
-
-
+            raise XrayException(e, sys)
