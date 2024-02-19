fa7a02af16705b5309687daaa1a1be17b8281001
ishawant kumar <ishawantkumar@gmail.com>
2024-02-19 09:23:33 +0530

    entity updated for data transformation

--- Xray/entity/artifact_entity.py
+++ Xray/entity/artifact_entity.py
@@ -7,3 +7,13 @@ class DataIngestionArtifact:
     train_file_path: str
 
     test_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_train_object: DataLoader
+
+    transformed_test_object: DataLoader
+
+    train_transform_file_path: str
+
+    test_transform_file_path: str
